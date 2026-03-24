230b1dd61d16384a6b350b20bbdde9f8f852b304
Aadit Yadav <aadityadav121@gmail.com>
2026-03-24 13:19:03 +0530

    Day 2 of JS done

---
+++ Javascript/Day-2/script.js
@@ -0,0 +1,23 @@
+let Fullname = "asdgasdgasg";
+let number = 7;
+let boolean = true;
+
+let a = null;
+let b;
+
+let arr = [80, 80, 80, 80, 80, 80];
+
+let arr2 = [80, "80", true, null, undefined, 80];
+
+console.log(Fullname);
+console.log(typeof Fullname);
+console.log(number);
+console.log(typeof number);
+console.log(boolean);
+console.log(typeof boolean);
+
+console.log(a);
+console.log(b);
+
+console.log(arr);
+console.log(arr2);
