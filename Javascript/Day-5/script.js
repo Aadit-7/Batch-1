f887cfacbe960856e26edb8df96b4da0adecff51
Aadit Yadav <aadityadav121@gmail.com>
2026-03-31 13:06:09 +0530

    Day-5 Done

---
+++ Javascript/Day-5/script.js
@@ -0,0 +1,27 @@
+/*
+if (condition) {
+    console.log("output");
+}
+ else{
+    console.log("Result");
+    }
+*/
+
+/*
+for (initialization; condition; increment) {
+    console.log("otuput")
+}
+
+*/
+
+
+/*
+
+let i;
+
+console.log(i);
+for (i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+*/
