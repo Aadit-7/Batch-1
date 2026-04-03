836c0e56cdabbf00a37c105d84d00c24d913ec37
Aadit Yadav <aadityadav121@gmail.com>
2026-04-03 13:10:19 +0530

    Day-8 Done

---
+++ Javascript/Day-8/script.js
@@ -0,0 +1,75 @@
+// function sayHello() {
+//   let a = 10;
+//   let b = 20;
+
+//   if (a > b) {
+//     let i;
+//     for (i = 0; i <= 10; i++) {
+//       console.log(i);
+//     }
+//   } else {
+//     let i;
+//     for (i = 1; i <= 10; i++) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// sayHello();
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// sayHello();
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// sayHello();
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+// console.log("first");
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// function subs(a, b) {
+//   console.log(a - b);
+// }
+// function mul(a, b) {
+//   console.log(a * b);
+// }
+// function div(a, b) {
+//   console.log(a / b);
+// }
+// function cal() {
+//   sum(2, 3);
+//   subs(3, 2);
+//   mul(2, 3);
+//   div(4, 2);
+// }
+// cal();
+
+// function sayMyName(name) {
+//   console.log(name);
+// }
+
+// sayMyName();
+
+// let num = +prompt("Enter number");
+
+// console.log(Math.ceil(num));
+// console.log(Math.floor(12.9))
+// console.log(Math.round(12.4));
+
+console.log(Math.trunc(12.56465465465465));
+console.log((864.5465897).toFixed(0));
