779283540fd7ee2dfe390730114537b0b36c2c72
Aadit Yadav <aadityadav121@gmail.com>
2026-04-17 12:58:29 +0530

    Dom day-1 Done

---
+++ DOM/Day-1/script.js
@@ -0,0 +1,9 @@
+let heading = document.querySelector(".heading");
+
+// heading.innerText = "This is javascript <i> something </i>";
+heading.innerHTML = "This is javascript <i> something </i>";
+heading.style.backgroundColor = "red";
+
+let heading2 = document.querySelectorAll("h2");
+
+console.log(heading2);
